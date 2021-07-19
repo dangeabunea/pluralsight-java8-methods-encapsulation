c7af2ae78b3a6f245c5824e7f8e8ce2ecffd331f
Dan Geabunea <dangeabunea@hotmail.com>
2021-07-19 14:04:51 +0300

    Demo2 complete folder

---
+++ 2_overloading/complete/src/main/java/com/pluralsight/Coordinate.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class Coordinate {
+    private final double x;
+    private final double y;
+
+    public Coordinate(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getX() {
+        return x;
+    }
+}
